ac3c3cf356aff84cbdfca347143cacf3dc8c86a9
ResZ <88366703+ReaseRZ@users.noreply.github.com>
2024-01-26 08:44:04 +0700

    Make little adjusment

--- prayer_time.py
+++ prayer_time.py
@@ -14,7 +14,10 @@ def get_data_pray_time():
 def pray_times():
     pray_times_data = []
     for name_time,time in get_data_pray_time():
-        pray_times_data.append("{} : {}".format(name_time,time))
+        pray_local_array = []
+        pray_local_array.append(name_time)
+        pray_local_array.append(time)
+        pray_times_data.append(pray_local_array)
     return pray_times_data
 
 def hijri_date():
