d80f39178075d8fb117cefe35d7a4298d30499b1
ReaseRZ <resoft.cpp@gmail.com>
2024-01-31 09:02:58 +0700

    Fix major bug (Memory Leak) and (Adzan sound) play in all time

--- output/IPray/_internal/location.py
+++
@@ -1,4 +0,0 @@
-CityList = [['Surabaya','Semarang','Jakarta','Medan','Pontianak','Jayapura','Padang'],
-            ['London','Liverpool','Oxford'],
-            ['Berlin','Stuttgart','Dortmund']]
-CountryList = ['Indonesia','United Kingdom','Germany']
