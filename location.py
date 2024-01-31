e692546e2549375072b638f4ec830493b05eb8e9
ResZ <88366703+ReaseRZ@users.noreply.github.com>
2024-01-31 10:39:47 +0700

    Add information

--- location.py
+++ location.py
@@ -1,3 +1,4 @@
+#You can add your location by yourself format 2D Array with same index CountryList[i]==CityList[i]
 CityList = [['Surabaya','Semarang','Jakarta','Medan','Pontianak','Jayapura','Padang'],
             ['London','Liverpool','Oxford'],
             ['Berlin','Stuttgart','Dortmund']]
