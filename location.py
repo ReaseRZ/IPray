cf99f24b7a54f08febc7e94b22e4cbb3fe5f3ca5
ResZ <88366703+ReaseRZ@users.noreply.github.com>
2024-03-29 14:05:30 +0700

    Now you can add the location yourself

--- location.py
+++ location.py
@@ -1,5 +1,7 @@
 #You can add your location by yourself format 2D Array with same index CountryList[i]==CityList[i]
+#Example CountryList = ['Japan'] you add the CityList as CityList=[['Tokyo','Osaka','Hokaido','Kyoto','Okinawa']]
 CityList = [['Surabaya','Semarang','Jakarta','Medan','Pontianak','Jayapura','Padang'],
             ['London','Liverpool','Oxford'],
-            ['Berlin','Stuttgart','Dortmund']]
-CountryList = ['Indonesia','United Kingdom','Germany']
+            ['Berlin','Stuttgart','Dortmund'],
+            ['Tokyo','Osaka','Hokaido','Kyoto','Okinawa']]
+CountryList = ['Indonesia','United Kingdom','Germany','Japan']
